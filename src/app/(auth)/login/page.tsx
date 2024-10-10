6d8fd7c14170cad31744939c32a35ca6d5978595
MinLion2k4 <135206246+MinLion2k4@users.noreply.github.com>
2024-10-10 16:46:07 +0700

    feat: login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 import { useAuth } from '@/context/authContext';
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-; import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { setCookie } from '@/helpers/cookies';
 
 import "./login.css";
